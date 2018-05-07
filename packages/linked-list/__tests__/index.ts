d8a7c0541726a06dfc0ce5d4c728f2aaa3a97d95
Jeff Wainwright <yowainwright@gmail.com>
2018-05-07 15:42:52 -0700

    [update] adds working/semi-functional tests

--- packages/linked-list/__tests__/index.ts
+++ packages/linked-list/__tests__/index.ts
@@ -1,11 +1,27 @@
 import { LinkedListNode, LinkedList } from '../index'
 
-// standard Jest is working test
+/*
+   Jest init
+   ---
+   - test Jest is working and
+   - linkedListNode and LinkedList are imported correctly
+*/
 test('Jest is working, LinkedListNode is imported', () => {
   console.info('TEST: files are being imported', LinkedListNode, LinkedList)
-  // expect(1).toBe(1)
   expect(typeof LinkedListNode).toBe('function')
   expect(typeof LinkedList).toBe('function')
-}
+})
 
+// test LinkedListNode init
+test('LinkedListNode init', function testLinkedListNodeInit () {
+  const node = new LinkedListNode('foo')
+  // expect(node).toBe({ value: 'foo', nextNode: null })
+  expect(typeof node).toEqual('object')
+  // expect(node.getNodeValue()).toEqual('foo')
+})
 
+// test LinkedListNode .getValue()
+test('LinkedListNode getValue', function testLinkedListNodeGetValue() {
+  const node = new LinkedListNode('foo')
+  expect(node.getNodeValue()).toEqual('foo')
+})
