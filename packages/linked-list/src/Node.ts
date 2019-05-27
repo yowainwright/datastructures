0dc222c284d2645776fbb93a1c52f34c923a3b6a
Jeff Wainwright <yowainwright@gmail.com>
2019-05-26 22:39:51 -0700

    fix(Node): simplifies Node

--- packages/linked-list/src/Node.ts
+++ packages/linked-list/src/Node.ts
@@ -1,68 +1,8 @@
 /**
  * Linked List Node
  */
-class Node<T> {
-  public name: string
-  public nextNode: Node<T> | null
-  public data: object | null
-
-  /**
-   * @param {string} name
-   * @param {object} data
-   */
-  constructor (name: string, data: object | null = null, nextNode: Node<T> | null = null) {
-    this.name = name
-    this.data = data
-    this.nextNode = nextNode
-  }
-
-  /**
-   * @returns {string} value
-   */
-  getNodeValue (): string {
-    return this.name
-  }
-
-  /**
-   * getNodeData
-   * returns the data of a Node
-   * @returns {object} data
-   */
-  getNodeData (): object {
-    return this.data
-  }
-
-  /**
-   * getNextNode
-   * returns nextNode
-   * @returns {Node} this.nextNode
-   */
-  getNextNode (): Node<T> | null {
-    return this.nextNode
-  }
-
-  /**
-   * setNodeValue
-   * sets a Node value
-   * @param {value} value
-   */
-  setNodeValue (name: string): void {
-    this.name = name
-  }
-
-  /**
-   * @param {object} data
-   */
-  setNodeData (data: object): void {
-    this.data = data
-  }
-
-  /**
-   * @param {Node} nextNode
-   */
-  setNextNode (nextNode: Node<T> | null): void {
-    this.nextNode = nextNode
-  }
-}
-
-export { Node }
+export const Node = (name: string, nextNode: object | null = null, data: object | null = null) => ({
+  name,
+  nextNode,
+  data,
+})
