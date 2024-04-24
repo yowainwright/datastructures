4e7866e02ab3c2f8c6c01515f1c7216f428b3b5d
Jeff Wainwright <1074042+yowainwright@users.noreply.github.com>
2024-04-24 16:28:10 -0700

    chore: updates eslint (#425)
    * chore: updates eslint
    
    * chore: move devDep

---
+++ eslint.config.mjs
@@ -0,0 +1,15 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+
+export default [
+  {
+     ignores: ["*.js", "**/dist"],
+  },
+  {
+    languageOptions: { globals: globals.browser },
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+];
